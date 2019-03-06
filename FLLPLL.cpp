bffd8d2a10a210f4ea966244478c061dc21233aa
W. Kyle Gilbertson <wkyleg@gmail.com>
2019-03-06 11:44:48 -0700

    FLLPLL converges after thousands of ms

---
+++ FLLPLL.cpp
@@ -0,0 +1,77 @@
+/* Filename: testNCO.cpp                                          2019-03-05 */
+// Verify NCO model works
+
+
+#include <cstdio>
+#include <cmath>
+#include "NCO.h"
+
+using namespace std;
+
+int main() {
+  long int idx=0, n, ref_i, out_i, index = 0;
+  float FSample = 38192000, RefFreq = 9548000, OutFreq = 9548100, PDItime = 0.001;
+  double dot = 0.0, cross = 0.0, FreqError = 5000.0, NewFreq = 0.0, LastFreq = 0.0;
+  double Error = 0.0, LastError = 0.0, Phi = 0.0, LastPhi = 0.0;
+  double  Wn = 47.14, Tau1 = 1/47.14, Tau2 = 2 * 0.707 / 47.14;
+  long int I1, I2, Q1, Q2;
+  I1 = I2 = Q1 = Q2 = 1;
+  NCO out (5, FSample);
+  NCO ref (5, FSample);
+  NCO a(5, FSample);
+  a.SetFrequency(9.5485e6);
+  out.SetFrequency(OutFreq);
+  ref.SetFrequency(RefFreq);
+NewFreq = OutFreq;
+LastFreq = RefFreq;
+/*  printf("length: %d mask: %3d dPhase: %3d\n", a.m_len, a.m_mask, a.m_dphase);
+  for (idx=0; idx<37; idx++) {
+    printf("%10lu %3d %9.3f %9.3f\n",
+    a.m_phase, index+1, a.m_sintable[index], a.m_costable[index]);
+    index = a.clk();
+  }*/
+
+while (abs(out.Frequency - RefFreq) > 1) {
+//  for (idx=0; idx<1000; idx++) {
+
+   for (n = 0; n<38192; n++) {
+    ref_i = ref.clk();
+    out_i = out.clk();
+    Q1 += (long int) (ref.m_costable[ref_i] * out.m_costable[out_i]);
+    I1 += (long int) (ref.m_sintable[ref_i] * out.m_costable[out_i]);
+   }
+   printf("I1%11ld Q1%11ld ", I1, Q1);
+//   if (I1 == 0) I1 = 1;
+   Error = atan(Q1/I1)/(2*M_PI);
+   Phi = LastPhi + Tau2/Tau1 * (Error - LastError) + Error * (PDItime/Tau1);
+   LastPhi = Phi;
+   LastError = Error;
+   out.SetFrequency(out.Frequency - Phi * 1.0);
+//    printf("Error: %9.3f Phi: %9.3f\n", Error, Phi);
+   for (n=0; n<38192; n++) {
+    ref_i = ref.clk();
+    out_i = out.clk();
+    Q2 += (long int) (ref.m_costable[ref_i] * out.m_costable[out_i]);
+    I2 += (long int) (ref.m_sintable[ref_i] * out.m_costable[out_i]);
+   }
+   printf("I2%11ld Q2%11ld ", I2, Q2);
+//   if (I2 == 0) I2 = 1;
+   Error = atan(Q2/I2)/(2*M_PI);
+   Phi = LastPhi + Tau2/Tau1 * (Error - LastError) + Error * (PDItime/Tau1);
+   LastPhi = Phi;
+   LastError = Error;
+   out.SetFrequency(out.Frequency - Phi * 1.0);
+//    printf("Error: %9.3f Phi: %9.3f\n", Error, Phi);
+    
+   dot   = I1 * I2 + Q1 * Q2;
+   cross = I1 * Q2 - I2 * Q1;
+   FreqError = atan2(cross, dot) / (2 * M_PI * PDItime);
+//   out.SetFrequency(OutFreq + FreqError/8);
+//   printf("%3ld %9.3f %9.0f\n", idx+1, FreqError, NewFreq);
+   printf("idx%5ld FErr:%9.3f F:%9.0f\n", idx++, out.Frequency - RefFreq, out.Frequency);
+   LastFreq = NewFreq;
+   NewFreq += FreqError * 1.0;
+//   out.SetFrequency(NewFreq);
+   I1=I2=Q1=Q2=1;
+  }
+}
--- NCO.cpp
+++ NCO.cpp
@@ -4,10 +4,6 @@
 #include <cmath>
 #include "NCO.h"
 
-// Adjust the sample rate for your implementation as necessary
-// const float ONE_ROTATION = (float) 2.0 * (1u << 31);
-//const float ONE_ROTATION = 2.0 * (1u << (sizeof(unsigned) * 8 - 1));
-
 NCO::NCO(const int lgtblsize, const float m_sample_clk) {
 	SAMPLE_RATE = m_sample_clk;
     // We'll use a table 2^(lgtblize) in length.  This is non-negotiable, as the
@@ -17,11 +13,11 @@ NCO::NCO(const int lgtblsize, const float m_sample_clk) {
 
     // m_mask is 1 for any bit used in the index, zero otherwise
     m_mask = m_len - 1;
-    m_sintable = new float[m_len];
-    m_costable = new float[m_len];
+    m_sintable = new char[m_len];
+    m_costable = new char[m_len];
     for (k = 0; k < m_len; k++) {
-        m_sintable[k] = (float) sin(2.0 * M_PI * k / (double) m_len);
-        m_costable[k] = (float) cos(2.0 * M_PI * k / (double) m_len);
+        m_sintable[k] = (char) (m_len) * sin(2.0 * M_PI * k / (double) m_len);
+        m_costable[k] = (char) (m_len) * cos(2.0 * M_PI * k / (double) m_len);
     }
     // m_phase is the variable holding our PHI[n] function from above.
     // We'll initialize our initial phase and frequency to zero
@@ -34,9 +30,10 @@ NCO::~NCO(void) {
     delete[] m_costable;
 }
 
-void NCO::frequency(float f) {
+void NCO::SetFrequency(float f) {
     // Convert the frequency to a fractional difference in phase
     m_dphase = (unsigned long)(f * ONE_ROTATION / SAMPLE_RATE);
+    Frequency = f;
 }
 
 unsigned NCO::clk(void) {
@@ -51,15 +48,15 @@ unsigned NCO::clk(void) {
 
     // Insist that this index be found within 0... (m_len-1)
     index &= m_mask;
-
+    idx = index;
     // Finally return the table lookup value
-	return index;
+	return idx;
 }
 
-float NCO::cosine(int idx) {
+char NCO::cosine(int idx) {
 	return m_costable[idx];
 }
 
-float NCO::sine(int idx) {
+char NCO::sine(int idx) {
 	return m_sintable[idx];
 }
