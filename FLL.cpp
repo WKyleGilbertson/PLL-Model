79c372cec0e1e6379883701b73608865b4a46e26
W. Kyle Gilbertson <wkyleg@gmail.com>
2019-03-14 09:47:06 -0600

    FLL seems to work, but PLL not so sure

--- FLL.cpp
+++ FLL.cpp
@@ -12,41 +12,44 @@ using namespace std;
 
 int main() {
   long int idx=0, n, ref_i, out_i, index = 0;
-  float FSample = 38192000, RefFreq = 9548000, OutFreq = 9548405, PDItime = 0.001;
+  float FSample = 38192000, RefFreq = 9548000, OutFreq = 9548235, PDItime = 0.001;
   double FreqError = 0.0, NewFreq = 0.0, cross, dot, Ips1, Qps1, Ips2, Qps2;
   long int I1, I2, Q1, Q2;
+  char SampleData;
 //  long long int cross, dot;
   I1 = I2 = Q1 = Q2 = 1;
-  NCO out (5, FSample);
-  NCO ref (5, FSample);
+  NCO out (5, FSample); // Can use more bits with internal sin/cos map
+  NCO ref (3, FSample); // Incoming signal commonly 2 or 3 bit
   NCO a(5, FSample);
   out.SetFrequency(OutFreq);
   ref.SetFrequency(RefFreq);
 
 //while (abs(LastFreq - NewFreq) > 100) {
-  for (idx=0; idx<25; idx++) {
+  for (idx=0; idx<10; idx++) {
 
    for (n = 0; n<FSample*PDItime; n++) {
     ref.clk();
     out.clk();
-    I1 += (ref.sine(ref.idx) * out.sine(out.idx));
-    Q1 += (ref.sine(ref.idx) * out.cosine(out.idx));
+    SampleData = ref.sine(ref.idx);
+    I1 += SampleData * out.sine(out.idx);
+    Q1 += SampleData * out.cosine(out.idx);
 //    if (n == 0) printf("%ld %ld", I1, Q1);
    }
-   Ips1 = I1 / (FSample*PDItime);
-   Qps1 = Q1 / (FSample*PDItime);
+   Ips1 = I1 / (FSample*PDItime);  // Normalize accumulated integration
+   Qps1 = Q1 / (FSample*PDItime);  // Convenient time to convert to double
+   
    for (n=0; n<FSample*PDItime; n++) {
     ref.clk();
     out.clk();
-    I2 += (ref.sine(ref.idx) * out.sine(out.idx));
-    Q2 += (ref.sine(ref.idx) * out.cosine(out.idx));
+    SampleData = ref.sine(ref.idx);
+    I2 += SampleData * out.sine(out.idx);
+    Q2 += SampleData * out.cosine(out.idx);
    }
    Ips2 = I2 / (FSample*PDItime);
    Qps2 = Q2 / (FSample*PDItime);
-   dot   = (Ips1 * Ips2 + Qps1 * Qps2);
-   cross = (Ips1 * Qps2 - Ips2 * Qps1);
-   //dot   = (double) (I1 * I2 + Q1 * Q2);
-   //cross = (double) (I1 * Q2 - I2 * Q1);
+   
+   dot   = Ips1 * Ips2 + Qps1 * Qps2;
+   cross = Ips1 * Qps2 - Ips2 * Qps1;
    FreqError = atan2(cross, dot) / (2 * M_PI * PDItime);
    NewFreq = out.Frequency + FreqError / 1.0;
    printf("%4ld CalcErr:%9.3f ActErr:%9.3f Fnow:%9.0f Fnxt:%7.0f\n",
--- FLLPLL.cpp
+++ FLLPLL.cpp
@@ -9,11 +9,13 @@ using namespace std;
 int main() {
   FILE * outfile = fopen("FLLPLL.dat", "w");
   long int idx=0, n, ref_i, out_i, index = 0;
-  float FSample = 38192000, RefFreq = 9548000, OutFreq = 9548005, PDItime = 0.001;
-  double dot = 0.0, cross = 0.0, FreqError = 5000.0, NewFreq = 0.0, LastFreq = 0.0;
+  float FSample = 38192000, RefFreq = 9548000, OutFreq = 9548450, PDItime = 0.001;
+  double dot = 0.0, cross = 0.0, FreqError = 5000.0, NewFreq = 0.0;
+  double Ips1, Ips2, Qps1, Qps2;
   double Error = 0.0, LastError = 0.0, Phi = 0.0, LastPhi = 0.0;
   double  Wn = 47.14, Tau1 = 1/(47.14*47.14), Tau2 = 2 * 0.707 / 47.14;
   long int I1, I2, Q1, Q2;
+  char SampleData;
   I1 = I2 = Q1 = Q2 = 1;
   NCO out (5, FSample);
   NCO ref (5, FSample);
@@ -21,49 +23,56 @@ int main() {
   a.SetFrequency(9.5485e6);
   out.SetFrequency(OutFreq);
   ref.SetFrequency(RefFreq);
-NewFreq = OutFreq;
-LastFreq = RefFreq;
+
   printf("Tau1: %5.3g Tau2: %5.3g Wn: %5.3g Ref: %7.0f\n",
           Tau1, Tau2, Wn, RefFreq);
 
-while (abs(out.Frequency - RefFreq) > 1 && abs(out.Frequency - RefFreq) < 1000) {
-   for (n = 0; n<38192; n++) {
+//while (abs(out.Frequency - RefFreq) > 1 && abs(out.Frequency - RefFreq) < 1000) {
+ for (idx=0; idx<10; idx++) {
+   for (n = 0; n<FSample*PDItime; n++) {
     ref.clk();
     out.clk();
-    I1 += (long int) (ref.cosine(ref.idx) * out.cosine(out.idx));
-    Q1 += (long int) (ref.sine(ref.idx)   * out.sine(out.idx));
+    SampleData = ref.sine(ref.idx);
+    I1 += SampleData * out.sine(out.idx);
+    Q1 += SampleData * out.cosine(out.idx);
    }
+   Ips1 = I1 / (FSample*PDItime);  // Normalize accumulated integration
+   Qps1 = Q1 / (FSample*PDItime);  // Convenient time to convert to double
 //   printf("I1%11ld Q1%11ld ", I1, Q1);
-   Error = atan(Q1/I1)/(2*M_PI);
+   Error = atan(Qps1/Ips1)/(2*M_PI);
+//   Error = atan(Q1/I1)/(2*M_PI);
    Phi = LastPhi + Tau2/Tau1 * (Error - LastError) + Error * (PDItime/Tau1);
    LastPhi = Phi;
    LastError = Error;
-   out.SetFrequency(out.Frequency - Phi * 1.0);
-   for (n=0; n<38192; n++) {
+//   out.SetFrequency(out.Frequency - Phi * 1.0);
+   
+   for (n=0; n<FSample*PDItime; n++) {
     ref.clk();
     out.clk();
-    I2 += (long int) (ref.cosine(ref.idx) * out.cosine(out.idx));
-    Q2 += (long int) (ref.sine(ref.idx)   * out.sine(out.idx));
+    SampleData = ref.sine(ref.idx);
+    I2 += SampleData * out.sine(out.idx);
+    Q2 += SampleData * out.cosine(out.idx);
    }
+   Ips2 = I2 / (FSample*PDItime);
+   Qps2 = Q2 / (FSample*PDItime);
 //   printf("I2%11ld Q2%11ld ", I2, Q2);
-   Error = atan(Q2/I2)/(2*M_PI);
+   Error = atan(Qps2/Ips2)/(2*M_PI);
+//   Error = atan(Q2/I2)/(2*M_PI);
    Phi = LastPhi + Tau2/Tau1 * (Error - LastError) + Error * (PDItime/Tau1);
    LastPhi = Phi;
    LastError = Error;
-   out.SetFrequency(out.Frequency - Phi * 1.0);
+//   out.SetFrequency(out.Frequency - Phi * 1.0);
+   dot   = Ips1 * Ips2 + Qps1 * Qps2;
+   cross = Ips1 * Qps2 - Ips2 * Qps1;
 
-   dot   = I1 * I2 + Q1 * Q2;
-   cross = I1 * Q2 - I2 * Q1;
    FreqError = atan2(cross, dot) / (2 * M_PI * PDItime);
-//   out.SetFrequency(OutFreq + FreqError/8);
-//   printf("%3ld %9.3f %9.0f\n", idx+1, FreqError, NewFreq);
+   NewFreq = out.Frequency + FreqError / 1.0;
+
    printf("%3d FErr:%9.3lf %c%c: %6.1lf %c:%5.1lf %c: %7.0f %7.0f\n",
           idx+1, FreqError, 235, 159, fabs(RefFreq - out.Frequency), 237, Phi, 
           159, out.Frequency, NewFreq); 
    fprintf(outfile, "%9.3f\n", out.Frequency - RefFreq);
-   LastFreq = NewFreq;
-   NewFreq += FreqError * 1.0;
-//   out.SetFrequency(NewFreq);
+   out.SetFrequency(NewFreq);
    I1=I2=Q1=Q2=1;
   }
   fclose(outfile);
